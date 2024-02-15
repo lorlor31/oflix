299ac961a2682d5ff9acdc71d554ed07720791f6
gregoclock <gregory@oclock.io>
2024-02-15 13:29:10 +0100

    ajout en favori + flash messages

--- sources/routes.md
+++ sources/routes.md
@@ -5,5 +5,6 @@
 | `/` | `GET`        | `MainController` | `home`  | Bienvenue sur O'flix | Page d'accueil |
 | `/movie` | `GET`        | `MovieController` | `list`  | Liste des films disponibles | liste de tous le films |
 | `/movie/{id}` | `GET`        | `MovieController` | `show`  | Détail du movie | détail du film dont l'id est fourni en url |
-| `/favorites` | `GET`        | `MovieController` | `favorite`  | Mes films favoris | Liste des films favoris sélectionnés par l'utilisateur |
+| `/favorites/{id}` | `GET`        | `MovieController` | `favoritesAdd`  | - | ajoute le film en session et redirige vers la page /favorites |
+| `/favorites` | `GET`        | `MovieController` | `favorites`  | Mes films favoris | Liste des films favoris sélectionnés par l'utilisateur |
 
--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Utils\Data;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -11,6 +12,34 @@ use Symfony\Component\Routing\Attribute\Route;
 class MovieController extends AbstractController
 {
 
+    #[Route('/favorites/{id<\d+>}', name: 'favorites_add', methods: ['GET'])]
+    public function favoritesAdd(int $id, Request $request): Response
+    {
+        // récupérer l'id de l'url done !
+        // récupérer le movie qui correspond à l'id
+        $movie = Data::getOneById($id);
+
+        // todo vérifier que movie n'est pas un tableau vide
+        // todo si c'est le cas que faire ? 
+
+        // ajouter le movie dans la session
+        $session = $request->getSession();
+        // récupérer les éventuels movies en sessions dans une variable $moviesInSession
+        $moviesInSession = $session->get('favorite_movies', []);
+
+        // ajouter le movie actuel dans le tableau $moviesInSession
+        $moviesInSession[$id] = $movie;
+
+        // réécrire le tableau dans la session ( à la meme clef )
+        $session->set('favorite_movies', $moviesInSession); 
+
+        $this->addFlash('success', 'Le film ' . $movie['title'] . ' a été ajouté avec succès');
+
+        // rediriger l'utilisateur sur la page /favorites
+        return $this->redirectToRoute('app_movie_favorites');
+
+    }
+    
     #[Route('/favorites', name: 'favorites', methods: ['GET'])]
     public function favorites(): Response
     {
