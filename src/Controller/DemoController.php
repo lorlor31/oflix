2ef3002f8e96bb5daa89d541c0dbb9bb7fc54167
gregoclock <gregory@oclock.io>
2024-02-15 11:41:29 +0100

    demo session

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DemoController
@@ -10,6 +13,7 @@ class DemoController
     #[Route('/demo/')]
     public function demo(): Response
     {
+
         $response = new Response();
 
         $response->setContent('<body><h1>Hello les Jellys</h1></body>');
@@ -39,9 +43,27 @@ class DemoController
     }
 
     #[Route('/demo/hello/{name}')]
-    public function hello(string $name): Response
+    public function hello(string $name, Request $request): Response
+    {
+        $session = $request->getSession();
+
+        $session->set('name', $name);
+        $response = new Response();
+
+        $response->setContent('<body><h1>Hello ' .  $name . '</h1></body>');
+        // this looks exactly the same
+        return $response;
+    }
+
+    #[Route('/demo/session/hello')]
+    public function sessionHello(Request $request): Response
     {
         $response = new Response();
+
+        $session = $request->getSession();
+        // récupérer le nom qui est dans la session
+        // et l'afficher dans le message
+        $name = $session->get('name', 'Inconnu des herbes rouges');
 
         $response->setContent('<body><h1>Hello ' .  $name . '</h1></body>');
         // this looks exactly the same
