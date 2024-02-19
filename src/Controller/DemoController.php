5e07f757d8e3b57c6c60a0f1e13f5154a6c25dcb
gregoclock <gregory@oclock.io>
2024-02-19 13:36:08 +0100

    lecture /écriture dans la BDD avec Doctrine

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Genre;
+use App\Repository\GenreRepository;
+use ContainerPub1CDv\getGenreRepositoryService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DemoController
+class DemoController extends AbstractController
 {
     #[Route('/demo/')]
     public function demo(): Response
     {
-
         $response = new Response();
 
         $response->setContent('<body><h1>Hello les Jellys</h1></body>');
@@ -32,6 +36,41 @@ class DemoController
         return $response;
     }
 
+    #[Route('/demo/doctrine')]
+    public function doctrine(EntityManagerInterface $entityManager): Response
+    {
+
+        $genre = new Genre();
+        $genre->setName(uniqid('genre-'));
+
+        // on demande à l'entity manager de prendre en compte cette entité
+        $entityManager->persist($genre);
+
+
+        // on demande à l'entity manager d'exécuter les requêtes
+        $entityManager->flush();
+
+        dd($genre);
+        return new Response();
+
+    }
+
+    #[Route('/demo/doctrine/read')]
+    public function doctrineRead(GenreRepository $genreRepository): Response
+    {
+        // pour récupérer tous les genres
+        $genreList = $genreRepository->findAll();
+        // il existe 4 méthodes par défaut : find($id), findAll(), findBy() findOneBy(=)
+        dump($genreList);
+        dump($genreRepository->findBy(['name' => 'genre-%']));
+        dump($genreRepository->find(1));
+        dump($genreRepository->find(-1));
+        dd($genreRepository->findOneBy(['name' => 'genre-65d344e30c925']));
+
+        // return new Response();
+
+
+    }
     #[Route('/demo/hello/{name}', methods: ['POST'])]
     public function testPost(string $name): Response
     {
@@ -71,5 +110,7 @@ class DemoController
         // this looks exactly the same
         return $response;
     }
+
+    
 
 }
